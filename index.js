33d0b9a3a733d6faedd74179b415957c3709407c
DaniyalSKKR <122335626+DaniyalSKKR@users.noreply.github.com>
2025-06-12 19:59:28 -0500

    version 2 of website, bootstrap implemented

--- index.js
+++ index.js
@@ -1,97 +1,61 @@
-function showeducation(){
-    $("#edu_container").css("display","inherit");
-    $("#edu_container").addClass("animated slideInRight");
-    $(".background-image").css("filter", "blur(5px)");
-    setTimeout(function(){
-        $("#edu_container").removeClass("animated slideInRight");
-    },900);
-}
-function closeeducation(){
-    $("#edu_container").addClass("animated slideOutRight");
-    $(".background-image").css("filter", "None");
-    $(".background-image").css("filter", "None");
-    setTimeout(function(){
-        $("#edu_container").removeClass("animated slideOutRight");
-        $("#edu_container").css("display","none");
-    },900);
-}
-function showabout(){
-    $("#about_container").css("display","inherit");
-    // $("#about_container").addClass("animated slideInLeft");
-    // setTimeout(function(){
-    //     $("#about_container").removeClass("animated slideInLeft");
-    // },900);
-    $("#about_container").addClass("animated slideInRight");
-    $(".background-image").css("filter", "blur(5px)");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideInRight");
-    },900);
-}
-function closeabout(){
-    $("#about_container").addClass("animated slideOutRight");
-    $(".background-image").css("filter", "None");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutRight");
-        $("#about_container").css("display","none");
-    },900);
-}
-function showwork(){
-    $("#work_container").css("display","inherit");
-    $("#work_container").addClass("animated slideInRight");
-    $(".background-image").css("filter", "blur(5px)");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideInRight");
-    },900);
-}
-function closework(){
-    $("#work_container").addClass("animated slideOutRight");
-    $(".background-image").css("filter", "None");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
-        $("#work_container").css("display","none");
-    },900);
-}
-function showcontact(){
-    $("#contact_container").css("display","inherit");
-    $("#contact_container").addClass("animated slideInRight");
-    $(".background-image").css("filter", "blur(5px)");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInRight");
-    },900);
-}
-function closecontact(){
-    $("#contact_container").addClass("animated slideOutRight");
-    $(".background-image").css("filter", "None");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutRight");
-        $("#contact_container").css("display","none");
-            },900);
-}
-function showresume(){
-    $("#resume_container").css("display","inherit");
-    $("#resume_container").addClass("animated slideInRight");
-    $(".background-image").css("filter", "blur(5px)");
-    setTimeout(function(){
-        $("#resume_container").removeClass("animated slideInRight");
-    },900);
-}
-function closeresume(){
-    $("#resume_container").addClass("animated slideOutRight");
-    $(".background-image").css("filter", "None");
-    setTimeout(function(){
-        $("#resume_container").removeClass("animated slideOutRight");
-        $("#resume_container").css("display","none");
-            },900);
-}
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-      $("#box").css("display","none");
-      $("#menu").removeClass("animated fadeIn"); //added
+// About Panel
+const sidebar = document.getElementById("aboutPanel");
+const openBtn = document.getElementById("openAboutBtn");
+const closeBtn = document.getElementById("closeAboutBtn");
+
+openBtn.addEventListener("click", () => {
+  sidebar.style.transform = "translateY(0)";
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.style.transform = "translateY(-100%)";
+});
+
+// Education panel
+const educationSidebar = document.getElementById("educationPanel");
+const openEducationBtn = document.getElementById("openEducationBtn");
+const closeEducationBtn = document.getElementById("closeEducationBtn");
+
+openEducationBtn.addEventListener("click", () => {
+  educationSidebar.style.transform = "translateY(0)";
+});
+closeEducationBtn.addEventListener("click", () => {
+  educationSidebar.style.transform = "translateY(-100%)";
+});
+
+// Work panel
+const workSidebar = document.getElementById("workPanel");
+const openWorkBtn = document.getElementById("openWorkBtn");
+const closeWorkBtn = document.getElementById("closeWorkBtn");
+
+openWorkBtn.addEventListener("click", () => {
+  workSidebar.style.transform = "translateY(0)";
+});
+closeWorkBtn.addEventListener("click", () => {
+  workSidebar.style.transform = "translateY(-100%)";
+});
+
+// Resume panel
+const resumeSidebar = document.getElementById("resumePanel");
+const openResumeBtn = document.getElementById("openResumeBtn");
+const closeResumeBtn = document.getElementById("closeResumeBtn");
+
+openResumeBtn.addEventListener("click", () => {
+  resumeSidebar.style.transform = "translateY(0)";
+});
+closeResumeBtn.addEventListener("click", () => {
+  resumeSidebar.style.transform = "translateY(-100%)";
+});
+
+setTimeout(function () {
+  $("#loading").addClass("animated fadeOut");
+  setTimeout(function () {
+    $("#loading").removeClass("animated fadeOut");
+    $("#loading").css("display", "none");
+    $("#box").css("display", "none");
+    $("#menu").removeClass("animated fadeIn"); //added
     //   $("#about").removeClass("animated fadeIn");
     //   $("#contact").removeClass("animated fadeIn");
     //   $("#work").removeClass("animated fadeIn");
-    },1000);
-},1500);
+  }, 1000);
+}, 1500);
